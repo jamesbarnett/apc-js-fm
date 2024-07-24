25785dd2d4bebc4541febb7b563a76c21db7372b
Jim Barnett <james.barnett.jr@gmail.com>
2024-07-24 03:16:34 -0500

    Worked on sharing feature

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+let shareButton = document.getElementById("share");
+shareButton.addEventListener("click", (e) => {
+  shareButton.style.display = "none";
+
+  let sharePopup = document.getElementById("share-popup");
+  sharePopup.style.display = "flex";
+
+  let avatar = document.querySelector(".author img:first-child");
+  avatar.style.display = "none";
+
+  let authorInfo = document.querySelector(".author > div");
+  authorInfo.style.display = "none";
+});
